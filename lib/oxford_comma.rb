970318c23ee15e37e74be038c9d847edf528c046
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-13 14:00:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+output = String.new
+if array.size == 1
+  output << array.join
+  output
+elsif array.size == 2
+  output << "#{array[0]} and #{array[1]}"
+  output
+else
+  size = array.size
+  for i in 0..array.size-2 do
+    output << "#{array[i]}, "
+  end
+  output << "and #{array[-1]}"
+  output
+end
 end
